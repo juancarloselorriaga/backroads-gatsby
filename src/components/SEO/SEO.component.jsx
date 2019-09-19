4f85543db3abcbd67379bdf50dbe8d2191b7d2f9
juancarloselorriaga <juancarloselorriaga@gmail.com>
2019-09-18 21:35:51 -0500

    facebook card added

--- src/components/SEO/SEO.component.jsx
+++ src/components/SEO/SEO.component.jsx
@@ -33,6 +33,18 @@ const SEO = ({ title, description }) => {
       <meta name="image" content={image} />
       /**
       |--------------------------------------------------
+      | Facebook Cards
+      |--------------------------------------------------
+      */
+      <meta property="og:url" content={siteUrl} />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={siteTitle} />
+      <meta property="og:description" content={siteDesc} />
+      <meta property="og:image" content={`${siteUrl}${image}`} />
+      <meta property="og:image:width" content="400"/>
+      <meta property="og:image:height" content="300" />
+      /**
+      |--------------------------------------------------
       | Twitter Card
       |--------------------------------------------------
       */
